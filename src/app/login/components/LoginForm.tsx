5b835a0b25a10876f14d79bab7d4064dfaf687f1
Ethan Wu <tastymooncakes@gmail.com>
2025-03-18 15:29:17 +0800

    adjusted login mobile responsive and styling

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -13,7 +13,6 @@ const LoginForm = () => {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         setError("");
-        setLoading(true);
 
         if (!email || !password) {
             console.log("error")
@@ -21,6 +20,7 @@ const LoginForm = () => {
             return;
         }
         try {
+            setLoading(true);
             const response  = await fetch('api/auth', {
                 method: "POST",
                 headers: {
@@ -39,31 +39,43 @@ const LoginForm = () => {
         }
     };
 
-    return <div className="flex justify-center items-center min-h-screen bg-black-100">
-        <form onSubmit={handleSubmit} className="bg-white p-8 rounded-lg shadow-md w-100">
-            <div className="mb-8">
-            <label className="block text-gray-700 py-2">Email:</label>
-                <input 
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className="w-full px-4 py-2 border border-gray-700 rounded-lg focus:outline-none focus:border-blue-500 text-gray-700"
-                />
+    return <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-violet-200 to-sky-500">
+        <div className="flex flex-col md:flex-row  justify-center bg-white p-8 rounded-lg shadow-md w-fill max-w-4xl">
+            <div className="w-full md:w-1/2 relative mr-8 mb-8 md:mb-0">
+                <div className="border border-gray-700 rounded-xl overflow-hidden">
+                    <img
+                        src="https://pokemon.wingzero.tw/assets/pokemon/025.png"
+                        alt="Pikachu Picture"
+                    />
+                </div>
             </div>
-            <div className="mb-8">
-                <label className="block text-gray-700 py-2">Password:</label>
-                <input 
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="w-full px-4 py-2 border border-gray-700 rounded-lg focus:outline-none focus:border-blue-500 text-gray-700"
-                />
+            <div className="w-full md:w-1/2 flex flex-col justify-center">
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-8">
+                    <label className="block text-gray-700 py-2">Email:</label>
+                        <input 
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="w-full px-4 py-2 border border-gray-700 rounded-lg focus:outline-none focus:border-blue-500 text-gray-700"
+                        />
+                    </div>
+                    <div className="mb-8">
+                        <label className="block text-gray-700 py-2">Password:</label>
+                        <input 
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="w-full px-4 py-2 border border-gray-700 rounded-lg focus:outline-none focus:border-blue-500 text-gray-700"
+                        />
+                    </div>
+                    <button type="submit" className="w-full px-4 py-2 bg-blue-500 text-white rounded-lg">
+                        {loading ? "Logging In..." : "Login"}
+                    </button>
+                    {error && <p className="w-full text-center text-red-500 py-2">{error}</p>}
+                </form>
             </div>
-            <button type="submit" className="w-full px-4 py-2 bg-blue-500 text-white rounded-lg">
-                {loading ? "Logging In..." : "Login"}
-            </button>
-            {error && <p className="w-full text-center text-red-500 py-2">{error}</p>}
-        </form>
+        </div>     
     </div>
 }
 
